66ad41f3c65340ae0d6e7171ce18fdf4253ca3de
ThanhLinh023 <vuongthanhlinh2003.contact@gmail.com>
2022-01-18 22:17:09 +0700

    C++

--- HelloWorld/Mang2chieu/bai6.cpp
+++ HelloWorld/Mang2chieu/bai6.cpp
@@ -13,7 +13,7 @@ void input(int a[][50], int &m, int &n)
 }
 int main()
 {
-    int m, n, a[50][50], dem (0);
+    int m, n, a[50][50], dem1(0), dem2(0);
     cout << "Nhap so dong : ";
     cin >> m;
     cout << "Nhap so cot : ";
@@ -25,13 +25,11 @@ int main()
         {   
             for (int j = 0; j < m; j++)
             {
-                if (a[j][j] != 1 && a[i][j] != 0)
-                {
-                    dem++;
-                }
+                if (i == j and a[j][j] != 1) dem1++;
+                if (i != j and a[i][j] !=0) dem2++;
             }
         }
-        if (dem == 0)
+        if (dem1 == 0 and dem2 == 0)
         {
             cout << "Day la ma tran don vi";
         }
