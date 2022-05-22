b77ca059fa6b757f56413b96008a7b1cd7b343a8
ThanhLinh023 <vuongthanhlinh2003.contact@gmail.com>
2022-05-22 10:33:23 +0700

    MIPS

--- ComputerArchitecture/Lab1/IOString.asm
+++ ComputerArchitecture/Lab1/IOString.asm
@@ -1,23 +1,23 @@
 .data
 s1: .asciiz "Nhap : "  
 s2: .asciiz "Xuat : "
-chuoi: .space 255 #Kh?i t?o b? nh? có tên chuoi
+chuoi: .space 255
 .text
-li $v0, 4 #Kh?i t?o $v0 có giá tr? 4
-la $a0, s1 #Kh?i t?o $a0 v?i ??a ch? c?a s1
-syscall #L?nh h? th?ng in ra "Nhap : "
+li $v0, 4
+la $a0, s1 
+syscall 
 
-li $v0, 8 #Kh?i t?o $v0 có giá tr? 8
-la $a0, chuoi #Kh?i t?o $a0 v?i ??a ch? cua bi?n chuoi
-li $a1, 256 #Kh?i t?o $a1 v?i giá tr? 256 t??ng ?ng v?i ?? dài 
-syscall #L?nh h? th?ng dùng ?? nh?p chu?i vào
+li $v0, 8 
+la $a0, chuoi 
+li $a1, 256 
+syscall 
 
-li $v0, 4 #Kh?i t?o $v0 có giá tr? 4
-la $a0, s2 #Kh?i t?o $a0 v?i ??a ch? c?a s2
-syscall #L?nh h? th?ng in ra "Xuat : "
+li $v0, 4 
+la $a0, s2 
+syscall 
 
-li $v0, 4 #Kh?i t?o $v0 có giá tr? 4
-la $a0, chuoi #Kh?i t?o $a0 v?i ??a ch? cua bi?n chuoi
-syscall #L?nh h? th?ng ?? in ra chu?i v?a nh?p
+li $v0, 4 
+la $a0, chuoi 
+syscall
 
 
--- ComputerArchitecture/Lab1/Sum2num.asm
+++ ComputerArchitecture/Lab1/Sum2num.asm
@@ -3,27 +3,27 @@ s1: .asciiz "Input a : "
 s2: .asciiz "Input b : "
 s3: .asciiz "Sum = "
 .text
-li $v0, 4 #Kh?i t?o $v0 b?ng 4
-la $a0, s1 #Kh?i t?o $a0 v?i ??a ch? c?a s1
-syscall #In ra dong "Input a : "
+li $v0, 4
+la $a0, s1 
+syscall 
 
-li $v0, 5 #Kh?i t?o $v0 b?ng 5
-syscall #L?nh h? th?ng dùng ?? nh?p s? nguyên
-move $t0, $v0 #Gán $t0 = $v0 ?? ??c s? nguyên
+li $v0, 5 
+syscall 
+move $t0, $v0 
 
-li $v0, 4 #Kh?i t?o $v0 b?ng 4
-la $a0, s2 #Kh?i t?o $a0 v?i ??a ch? c?a s2
-syscall #In ra dong "Input b : "
+li $v0, 4 
+la $a0, s2 
+syscall 
 
-li $v0, 5 #Kh?i t?o $v0 b?ng 5
-syscall #L?nh h? th?ng dùng ?? nh?p s? nguyên
-move $t1, $v0 #Gán $t1 = $v0 ?? ??c s? nguyên
+li $v0, 5 
+syscall 
+move $t1, $v0 
 
-li $v0, 4 #Kh?i t?o $v0 b?ng 4
-la $a0, s3 #Kh?i t?o $a0 v?i ??a ch? c?a s3
-syscall #In ra dòng "Sum = "
+li $v0, 4 
+la $a0, s3 
+syscall 
 
-li $v0, 1 #Kh?i t?o $v0 b?ng 1
-add $t0, $t0, $t1 #Phép c?ng $t0 = $t0 + $t1
-la $a0, ($t0) #Kh?i t?o $a0 v?i ??a ch? $t0
-syscall #L?nh in ra k?t qu? phép c?ng
+li $v0, 1 
+add $t0, $t0, $t1 
+la $a0, ($t0) 
+syscall
