dac83630934ef7556c491213221962fc41660fb8
ThanhLinh023 <vuongthanhlinh2003.contact@gmail.com>
2023-03-24 17:01:45 +0700

    Day 2

---
+++ 30DaysOfJS/Day2/level2.js
@@ -0,0 +1,13 @@
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+console.log(typeof('10') == 10);
+console.log(Math.ceil(parseFloat('9.8')) == 10);
+console.log("python".includes('on'), "jargon".includes('on'));
+console.log("I hope this course is not full of jargon".includes('jargon'));
+console.log((Math.random()) * 100)
+console.log(((Math.random()) * 51) + 50)
+console.log((Math.random()) * 256)
+let text = 'JavaScript'
+console.log(text[Math.round(Math.random() * 10)])
+console.log('1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125')
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.substring(sentence.indexOf('because'), sentence.lastIndexOf('e') + 1));
